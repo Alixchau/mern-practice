a922ceec6037ed614acc421fb713557c72ef564f
Alix Shuk Han Chau <86642948+Alixchau@users.noreply.github.com>
2022-06-11 15:56:00 -0600

    Goals controller and routes set up

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const express = require('express')
+const app = express();
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const port = process.env.PORT
+const goalRoutes = require('./routes/goalRoutes');
+app.use(express.json());//enable app to pass json
+app.use(express.urlencoded({ extended: false }))
+app.use('/api/goals', goalRoutes);
+app.use(errorHandler) //overwirte the defaule express error handler
+
+app.listen(port, () => console.log(`server started on port ${port}`))
